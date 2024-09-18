99c08b7f9ad4f6f08dee3fd79d1a77750099a710
RahulKumar1998 <rahulkumar08.work@gmail.com>
2024-09-18 11:58:32 +0530

    singleton: thread-safe using double-checked locking

--- src/main/java/org/example/creational/Singleton.java
+++ src/main/java/org/example/creational/Singleton.java
@@ -4,15 +4,23 @@ public class Singleton {
     //Thread-safe singleton
     //instance created only when required
     private static Singleton instance;
+
     private Singleton() {
     }
 
-    //synchronized method ensure thread-safety
-    public static synchronized Singleton getInstance() {
+    //synchronized getInstance() using double-check locking
+    public static Singleton getInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
         }
         return instance;
     }
-    //synchronized method although thread-safe takes up lots of overhead and can reduce the speed of a large program/application
+
+    //Lock is acquired on class object as monitor object(Singleton.class)
+    //double-checked locking is used, if instance is null, locks are not acquired by the threads.
+    //this slightly reduces the overhead due to synchronization
 }
